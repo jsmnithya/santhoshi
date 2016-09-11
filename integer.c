3d40b628b2bcde2de0c0e7fde7a7eff1127cc6f9
jsmnithya <santhoshinithya@gmail.com>
2016-09-11 22:46:26 +0530

    integer c program

---
+++ integer.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter a number");
+scanf("%d",&a);
+if(a<0)
+{
+printf("it is a negative integer");
+}
+else if(a==0)
+{
+printf("it is a zero");
+}
+else if(a>0)
+{
+printf("it is a positive integer");
+}}
+
