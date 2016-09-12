c09a6805952cf173d0bd958500a766bd3377b210
jsmnithya <santhoshinithya@gmail.com>
2016-09-12 20:07:01 +0530

    my even or odd program

---
+++ even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter a number");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("it is a even number");
+}
+
+if(a%2!=0)
+{
+printf("it is a odd number");
+}
+
+}
